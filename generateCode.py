6ba57d5c8daae1f063c9d8c3cff5247e116268f2
David Lai <david.lai@redcypress.com.tw>
2023-05-31 17:54:47 +0800

    adjust logic to allow longer prompt inputs and generate angular code

--- generateCode.py
+++ generateCode.py
@@ -16,9 +16,9 @@ load_dotenv()
 
 # Initialize the FigmaFileLoader with environment variables
 figma_loader = FigmaFileLoader(
-    os.environ.get('ACCESS_TOKEN'),
-    os.environ.get('NODE_IDS'),
-    os.environ.get('FILE_KEY'),
+    os.environ.get("ACCESS_TOKEN"),
+    os.environ.get("NODE_IDS"),
+    os.environ.get("FILE_KEY"),
 )
 
 # Create an index and retriever for Figma documents
@@ -26,16 +26,21 @@ index = VectorstoreIndexCreator().from_loaders([figma_loader])
 figma_doc_retreiver = index.vectorstore.as_retriever()
 
 # Define system and human prompt templates
-system_prompt_template = """You are a senior developer.
-Use the provided design context to create idiomatic HTML/CSS code based on the user request.
-Everything must be inline in one file and your response must be directly renderable by the browser.
+user_prompt = input("Enter your prompt: ")
+
+system_prompt_template = """Act as senior web developer who is an expert in the Angular framework.
+Use the provided design context to create idiomatic HTML/CSS/TypeSCript code based on the user request.
+Everything must be output in one file.
 Write code that matches the Figma file nodes and metadata as exactly as you can.
 Figma file nodes and metadata: {context}"""
 
-human_prompt_template = "Code the {text}. Ensure that the code is mobile responsive and follows modern design principles."
+human_prompt_template = "Code this request: {user_prompt}. Ensure that the code is mobile responsive and follows modern design principles."
 
 # Initialize the ChatOpenAI model
-gpt_4 = ChatOpenAI(temperature=.03, model_name='gpt-3.5-turbo', request_timeout=120)
+gpt_model = ChatOpenAI(
+    temperature=0.03, model_name="gpt-3.5-turbo", request_timeout=120
+)
+
 
 # Define a function to generate code based on the input
 def generate_code(input):
@@ -43,37 +48,46 @@ def generate_code(input):
     relevant_nodes = figma_doc_retreiver.get_relevant_documents(input)
 
     # Create system and human message prompts
-    system_message_prompt = SystemMessagePromptTemplate.from_template(system_prompt_template)
-    human_message_prompt = HumanMessagePromptTemplate.from_template(human_prompt_template)
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+        system_prompt_template
+    )
+    human_message_prompt = HumanMessagePromptTemplate.from_template(
+        human_prompt_template
+    )
 
     # Create the chat prompt using the system and human message prompts
     conversation = [system_message_prompt, human_message_prompt]
     chat_prompt = ChatPromptTemplate.from_messages(conversation)
-    response = gpt_4(chat_prompt.format_prompt(
-        context=relevant_nodes,
-        text=input).to_messages())
+    response = gpt_model(
+        chat_prompt.format_prompt(
+            context=relevant_nodes, user_prompt=user_prompt
+        ).to_messages()
+    )
 
     return response.content
 
-# Add argument parsing to allow for command-line input
-parser = argparse.ArgumentParser(description='Generate HTML/CSS code based on input.')
-parser.add_argument('input_text', type=str, help='The input text for generating code.')
-args = parser.parse_args()
 
-# Generate the code using the input argument
-response = generate_code(args.input_text)
+# # Add argument parsing to allow for command-line input
+# parser = argparse.ArgumentParser(
+#     description="Generate HTML/CSS/TypeScript code based on input."
+# )
+# parser.add_argument("input_text", type=str, help="The input text for generating code.")
+# args = parser.parse_args()
+
+# Generate the code using the input prompt
+response = generate_code(user_prompt)
 
 # Define the output file name
-file_name = "output.html"
+file_name = "output.txt"
 
 # Save the generated code to the output file
 with open(file_name, "w") as file:
     file.write(response)
-    print(f"HTML file saved as {file_name}")
+    print(f"Output file saved as {file_name}")
 
-# Open the HTML file in the default web browser
-webbrowser_open_successful = webbrowser.open(file_name)
-if webbrowser_open_successful:
-    print("HTML file opened successfully in the default web browser.")
-else:
-    print("Failed to open the HTML file in the default web browser.")
+# # Open the HTML file in the default web browser
+# webbrowser_open_successful = webbrowser.open(file_name)
+# if webbrowser_open_successful:
+#     print("HTML file opened successfully in the default web browser.")
+# else:
+#     print("Failed to open the HTML file in the default web browser.")
